43231b65d4507ea1ebfd18936062bae3819e9f3a
Grupith <dylankoss@Dylans-Laptop.local>
2024-12-04 01:10:00 -0600

    removed inter font from layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,12 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Inter, Figtree } from "next/font/google";
+import { Figtree } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "Kamodo",
   description: "All-In-One Small Business Management Software",
 };
-
-// If loading a variable font, you don't need to specify the font weight
-const inter = Inter({
-  subsets: ["latin"],
-  display: "swap",
-});
 
 const figTree = Figtree({
   subsets: ["latin"],
