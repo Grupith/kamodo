dcf56566edb92c7f6d55fd79d2d594f58c1d84b6
Grupith <dylanhkoss@gmail.com>
2024-12-12 10:53:09 -0600

    linked up logout on dashboard

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -16,6 +16,7 @@ import {
   UsersIcon,
   BuildingOffice2Icon,
 } from "@heroicons/react/24/outline";
+import { signOutUser } from "@/lib/auth";
 
 const MOBILE_BREAKPOINT = 768; // Example breakpoint for mobile
 
@@ -291,6 +292,7 @@ export default function DashboardLayout({
                         </div>
                         <a
                           href="#"
+                          onClick={signOutUser}
                           className="block px-4 py-2 text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 transition"
                         >
                           Logout
